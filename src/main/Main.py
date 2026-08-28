bc6b27f42c9bb1122fff357320890b50577336ae
coolkevhi <coolkevhi@gmail.com>
2026-08-27 21:05:00 -0400

    Main Menu and User data

---
+++ src/main/Main.py
@@ -0,0 +1,7 @@
+from src.ui.menus.MainMenu import MainMenu
+
+#Runs the welcome message
+MainMenu.welcomeMessage()
+
+#Runs the main menu
+MainMenu.mainMenu(False)
---
+++ src/ui/menus/MainMenu.py
@@ -0,0 +1,25 @@
+import os
+
+class MainMenu:
+    #Welcome message only for when program starts for first time
+    def welcomeMessage():
+        print()
+        print("Welcome to the Straw Hat Pirates Database!")
+
+    #Main Menu method for the main menu of the program
+    def mainMenu(error): #error is true when the user puts a wrong option
+        print()
+        print("=======STRAW HAT PIRATES DATABASE=======")
+        print()
+        print("[1] Login")
+        print("[2] Quit")
+        if error: print("\033[3m" + "Please put an available option" + "\033[3m")
+        choice = input("")
+        if choice == "1":
+            print("")
+        elif choice == "2":
+            print("Thanks for using the program!")
+            SystemExit()
+        else:
+            os.system("cls")
+            MainMenu.mainMenu(True)
